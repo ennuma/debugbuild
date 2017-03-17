3ae53888a4e56e78311789b5e56c7b4750b3f36b
ennuma <zeng0129@gmail.com>
2017-03-17 14:35:30 -0700

    * setup 02

--- Source/GameSceneModule.cpp
+++ Source/GameSceneModule.cpp
@@ -2,16 +2,46 @@
 
 GameSceneModule::GameSceneModule()
 {
-
+	for (int i = 0; i < 100; i++)
+	{
+		Actor* pActor = new Actor();
+		actorPool.push_back(pActor);
+		actorUpdateList.push_back(pActor);
+	}
 }
 
 GameSceneModule::~GameSceneModule()
 {
-
+	for each (Actor* pActor in actorPool)
+	{
+		delete pActor;
+	}
 }
 
 void GameSceneModule::WorldTick(time_t deltaTime)
 {
 
+
+}
+
+//Actor class
+Actor::Actor()
+{
+
+}
+
+Actor::~Actor()
+{
+
+}
+
+//Region class
+Region::Region()
+{
+
+}
+
+Region::~Region()
+{
 
 }
--- Source/GameSceneModule.h
+++ Source/GameSceneModule.h
@@ -1,6 +1,7 @@
 #ifndef GAMESCENEMODULE
 #define GAMESCENEMODULE
 #include <time.h>
+#include <vector>
 class GameSceneModule
 {
 public:
@@ -8,6 +9,53 @@ public:
     ~GameSceneModule();
 	void WorldTick(time_t deltaTime);
 private:
+	std::vector<Actor*> actorUpdateList;
+	std::vector<Actor*> actorPool;
+};
 
+class Actor
+{
+public:
+	Actor();
+	~Actor();
+};
+
+class Region
+{
+public:
+	Region();
+	~Region();
+};
+
+enum EActionType
+{
+	EAction_Move,
+	EAction_Duel
+};
+
+struct Action
+{
+	EActionType eAction;
+	Actor* pOrigActor;
+};
+
+struct MoveAction : Action
+{
+	MoveAction(Region* _destRegion)
+	{
+		eAction = EActionType::EAction_Move;
+		pDestRegion = _destRegion;
+	}
+	Region* pDestRegion;
+};
+
+struct DuelAction : Action
+{
+	DuelAction(Actor* _actor)
+	{
+		eAction = EActionType::EAction_Duel;
+		pDestActor = _actor;
+	}
+	Actor* pDestActor;
 };
 #endif
